f47b2db4b83b4e3e1a6efeb01327dad63bb14691
Pedestal19 <gabeoji01@gmail.com>
2020-01-20 16:33:50 +0100

    directives task solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment1';
+  bgColor: string;
+
+  ngOnInit() {
+    this.bgColor = 'yellowgreen';
+  }
+
+  changeBackground(color: string) {
+    this.bgColor = color;
+  }
+
+  changeBackgroundRandomly() {
+    let r = Math.round(Math.random() * 255);
+    let g = Math.round(Math.random() * 255);
+    let b = Math.round(Math.random() * 255);
+    this.bgColor = `rgb(${r}, ${g}, ${b})`;
+  }
 }
